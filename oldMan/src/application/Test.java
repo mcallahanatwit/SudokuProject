c52ddfa2fc2dd813870706a4c27bf76197073d2b
mcallahanatwit <65292322+mcallahanatwit@users.noreply.github.com>
2020-08-10 16:10:17 -0400

    Add files via upload
    Data entry validation code base using the Element extended off of Grid class

---
+++ oldMan/src/application/Test.java
@@ -0,0 +1,13 @@
+package application;
+import application.Element;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Element.test();
+		// TODO Auto-generated method stub
+
+	}
+
+}
